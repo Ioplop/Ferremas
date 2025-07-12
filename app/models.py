964504cf8697cb9f923e41130a71fceecbf6fe98
Ioplop <ishk909@gmail.com>
2025-07-12 15:00:21 -0400

    Agregar modelos de cotizacion y orden de compra

--- app/models.py
+++ app/models.py
@@ -7,3 +7,37 @@ class Producto(db.Model):
     precio = db.Column(db.Float, nullable=False)
     stock = db.Column(db.Integer, nullable=False)
     imagen = db.Column(db.String(255))  # Ruta del archivo
+
+class Cotizacion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fecha = db.Column(db.DateTime, nullable=False)
+    productos = db.relationship("CotizacionProducto", back_populates="cotizacion", cascade="all, delete-orphan")
+
+class CotizacionProducto(db.Model):
+    __tablename__ = 'producto_cotizacion'
+    cotizacion_id = db.Column(
+        db.Integer, 
+        db.ForeignKey('cotizacion.id', ondelete="CASCADE"), 
+        primary_key=True
+    )
+    producto_id = db.Column(
+        db.Integer, 
+        db.ForeignKey('producto.id', ondelete="CASCADE"), 
+        primary_key=True
+    )
+    
+    cantidad = db.Column(db.Integer, nullable=False)
+
+    producto = db.relationship("Producto", passive_deletes=False)
+    cotizacion = db.relationship("Cotizacion", back_populates="productos")
+
+class OrdenCompra(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cotizacion_id = db.Column(db.Integer, db.ForeignKey('cotizacion.id'), nullable=False, unique=True)
+    
+    fecha_envio = db.Column(db.DateTime)
+    direccion_entrega = db.Column(db.String(200))
+    estado = db.Column(db.String(50), default='pendiente')
+
+    cotizacion = db.relationship("Cotizacion", back_populates="orden_compra")
+    
